9a88e9cac13d9cec33dacf898db1f328e5f54a10
Andreas Vibeto <andreas.vibeto@nordicsemi.no>
2024-05-27 15:46:26 +0200

    stuff that might be needed

---
+++ applications/nrf5340_audio/sysbuild.cmake
@@ -0,0 +1,2 @@
+add_overlay_dts(mcuboot ${ZEPHYR_NRF_MODULE_DIR}/applications/nrf5340_audio/ext-flash.overlay)
+add_overlay_dts(nrf5340_audio ${ZEPHYR_NRF_MODULE_DIR}/applications/nrf5340_audio/ext-flash.overlay)
